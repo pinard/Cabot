db0e9b71e827784a500df3289be531da32cf15d3
François Pinard <pinard@iro.umontreal.ca>
2009-07-01 22:02:48 -0400

    Web site

---
+++ web/site.mk
@@ -0,0 +1,10 @@
+package_name = Cabot
+long_package_name = "Personal IRC Bot"
+rootdir = $(HOME)/entretien/cabot/web
+margin_color = "\#cccccc"
+caption_color = "\#f1e4eb"
+
+SITE_ROOT = 1
+
+TEMPLATE = $(HOME)/entretien/mes-sites/gabarit.html
+LOGOURL = "/logo.png"
