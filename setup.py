7ee30cb5468e2a48743d51ad7185d186579c5a5f
François Pinard <pinard@iro.umontreal.ca>
2009-07-06 06:58:21 -0400

    setup.py: Remove period at end of description

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 # Copyright © 2004 PROGICIELS Bourbeau-Pinard, inc. Montréal.
 # François Pinard <pinard@progiciels-bpi.ca>, 2004.
 
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(name='Cabot',
       version='0.0',
-      description="IRC bot for `icule'.",
+      description="IRC bot for `icule'",
       author='François Pinard',
       author_email='pinard@iro.umontreal.ca',
       url='http://www.iro.umontreal.ca/~pinard',
