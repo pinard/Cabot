1b15ac18d578f1e548a52f886b0dbcbb697fe395
François Pinard <pinard@iro.umontreal.ca>
2009-07-18 13:40:59 -0400

    site.mk: Declare rootdir at top level

--- web/notes/site.mk
+++ web/notes/site.mk
@@ -1,6 +1,6 @@
 package_name = Cabot
 long_package_name = "Personal IRC Bot"
-rootdir = $(HOME)/entretien/cabot/web/notes
+rootdir = $(HOME)/entretien/cabot/web
 
 TEMPLATE = $(HOME)/entretien/mes-sites/gabarit.html
 LOGOURL = "/logo.png"
